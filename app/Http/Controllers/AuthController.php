36b222fdbe1041708deedc1184c15893a71cc3f0
vito-ctrl <aymane.elkhadraoui1@gmail.com>
2026-03-10 09:32:22 +0000

    auth (login and register) with sunctum

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $request->validate([
+            'name' => 'required|string|max:250',
+            'email' => 'required|string|email|max:250|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+            
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+            
+        return response()->json([
+            'message' => 'user registred succefully',
+            'user' => $user,
+        ], 201);
+    }
+
+    public function login(Request $request){
+        $request->validate([
+            'email' => 'required|string',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if(!$user || !Hash::check($request->password, $user->password)){
+            return response()->json([
+                'message' => 'Invalid credentials', 
+            ], 401);
+        }
+        
+        $token = $user->createToken('auth_token')->plainTextToken;
+        
+        return response()->json([
+            "user" => $user,
+            "token" => $token
+        ], 202);
+    }
+}
