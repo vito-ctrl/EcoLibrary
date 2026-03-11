19eae3002700e3a4a154bbb8bade450edd7b1259
vito-ctrl <aymane.elkhadraoui1@gmail.com>
2026-03-11 03:53:51 +0000

    book index methods for shoing all books

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -7,8 +7,18 @@ use App\Models\Book;
 
 class BookController extends Controller
 {
-    // public function index() {
-    //     $books = Book::all();
-    //     if(!$) 
-    // }
+    public function index() {
+        $books = Book::all();
+        if(!$books){
+            return response()->json([
+                "success" => false,
+                "message" => "no book founded",
+            ]);
+        } 
+
+        return response()->json([
+            "success" => true,
+            "data" => $books
+        ]);
+    }
 }
